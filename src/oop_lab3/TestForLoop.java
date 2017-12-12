03488614064a677145db92a45a6e883efe543c08
suwadeeAnantranon <sai_love_007@hotmail.com>
2017-12-12 10:26:43 +0700

    TestForLoop

---
+++ src/oop_lab3/TestForLoop.java
@@ -0,0 +1,29 @@
+package oop_lab3;
+
+public class TestForLoop {
+    public  static void  main (String[] args){
+//       for (int i=1;i<=10;i++)
+//          System.out.println("Hello.");
+//      for (int i=1;i<=10;i++)
+//           System.out.println(i);
+
+//       for (int i=1;i<=100;i++)
+//            System.out.print(i+ " ");
+
+//       for (int i=1;i<=100;i++) {
+//          if (i % 2 == 0)
+//              System.out.print(i + " ");
+//       }
+
+
+       for (int i=1;i<=100;i++) {
+            if (i % 3==0 && i % 5==0)
+                System.out.print(i + " ");
+        }
+
+
+
+
+    }//main
+
+}//class
