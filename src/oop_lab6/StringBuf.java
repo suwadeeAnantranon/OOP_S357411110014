b81e4b4541b265f802364897cc6b1d1e054f1de3
suwadeeAnantranon <sai_love_007@hotmail.com>
2017-12-27 18:13:28 +0700

    StringBuf

---
+++ src/oop_lab6/StringBuf.java
@@ -0,0 +1,22 @@
+package oop_lab6;
+
+public class StringBuf {
+    public static void main(String[] args){
+       // StringBuffer
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("Hello");
+        buffer.append(" Welcome");
+        buffer.append(" to");
+        buffer.append(" RMUTSV");
+        System.out.println(buffer);
+        System.out.println(buffer.length());
+        buffer.replace(0,5, "OOP");
+        System.out.println(buffer);
+        System.out.println(buffer.reverse());
+        buffer.delete(0,3);
+        System.out.println(buffer);
+
+
+    }//main
+
+}//class
